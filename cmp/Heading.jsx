64ae8e96ff01b561c09ce3504a2507e44eeeff71
nekron2323 <goncharov.uv@gmail.com>
2024-09-19 08:43:06 +0300

    heading onClick

--- cmp/Heading.jsx
+++ cmp/Heading.jsx
@@ -1,6 +1,6 @@
-const Heading = ({ tag, text }) => {
+const Heading = ({ tag, text, onClick }) => {
     const Tag = tag || 'h1';
-    return <Tag>{text}</Tag>
+    return <Tag onClick={onClick}>{text}</Tag>
 }
 
 export default Heading
